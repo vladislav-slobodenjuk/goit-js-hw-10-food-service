d096802afa281b8fc4f9428095b527e3f0d3c307
vladislav-slobodenjuk <vladjun@gmail.com>
2021-09-26 01:30:52 +0300

    createMarkup stage

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,6 +1,20 @@
 import refs from './refs';
-
 const { menu } = refs;
 
+import Handlebars from 'handlebars';
+
+import dishList from '../menu.json';
+// console.log(dishList);
+
 import itemMarkup from '../templates/list-item-markup.hbr';
-// console.log(itemMarkup);
+console.log(itemMarkup);
+
+const makeListItem = Handlebars.compile(itemMarkup);
+console.log(makeListItem);
+
+const listMarkup = dishList.map(dish => {
+  makeListItem(dish);
+});
+console.log(listMarkup);
+
+// menu.insertAdjacentHTML('beforeend', listMarkup);
