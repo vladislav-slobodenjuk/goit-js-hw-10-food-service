5f453fca631cb081aa0875e6d1c3d88303e522e0
vladislav-slobodenjuk <vladjun@gmail.com>
2021-10-01 19:15:10 +0300

    changeTheme updt3

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -3,21 +3,21 @@ import { toggle, body } from './refs';
 import { Theme } from './themes';
 const { LIGHT: light, DARK: dark } = Theme;
 
-// let storageTheme = localStorage.getItem('theme') || light;
-let storageTheme = localStorage.getItem('theme');
+let storageTheme = localStorage.getItem('theme') || light;
 
-if (!storageTheme) {
-  storageTheme = light;
-  localStorage.setItem('theme', light);
-}
+// if (!storageTheme) {
+//   storageTheme = light;
+//   localStorage.setItem('theme', light);
+// }
 
 body.classList.add(storageTheme);
 toggle.checked = storageTheme === dark;
 
-toggle.addEventListener('change', () => {
+toggle.addEventListener('change', e => {
   body.classList.toggle(dark);
   body.classList.toggle(light);
 
-  const theme = localStorage.getItem('theme') === light ? dark : light;
+  // const theme = localStorage.getItem('theme') === light ? dark : light;
+  const theme = e.target.checked ? dark : light;
   localStorage.setItem('theme', theme);
 });
